f4504481603603b4c479d10a451b90d3edec1657
Abygail27 <abygaailrrocha@gmail.com>
2019-07-01 12:08:16 -0500

    Updates

---
+++ precache-manifest.da16d1ab3580959b59d6258434160a13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3ff360a33b658049bf643c6b66e5b9c",
+    "url": "/Abygail27/arkano-dashboard-/index.html"
+  },
+  {
+    "revision": "9aa022d4318c74b5b6dd",
+    "url": "/Abygail27/arkano-dashboard-/static/css/main.0540c1e3.chunk.css"
+  },
+  {
+    "revision": "2e5ebc107be778fe6622",
+    "url": "/Abygail27/arkano-dashboard-/static/js/2.51e0fc97.chunk.js"
+  },
+  {
+    "revision": "9aa022d4318c74b5b6dd",
+    "url": "/Abygail27/arkano-dashboard-/static/js/main.70b91896.chunk.js"
+  },
+  {
+    "revision": "3f0ca44630eac4be2ce6",
+    "url": "/Abygail27/arkano-dashboard-/static/js/runtime~main.f12c5773.js"
+  }
+]);
